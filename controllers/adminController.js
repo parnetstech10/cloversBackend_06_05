b4231dcdb0f1637fa5b5f0e05df33df9f8a49d1c
parnets <parnetstech10@gmail.com>
2025-06-26 16:34:41 +0530

    priacyolicy

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -16,7 +16,7 @@ export const login = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
-};
+}; 
 
 export const register = async (req, res) => {
     const { username, password } = req.body;
