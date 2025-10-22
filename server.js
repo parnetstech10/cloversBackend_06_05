4bbfad79a163e8f4e823b789ffd0144c65f479d7
Parnets13 <parnetstech13@gmail.com>
2025-10-22 16:06:29 +0530

    this is commit

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.get('*', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
